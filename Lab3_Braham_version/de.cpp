fd8e2ab1e575795e0edf305f1d7a09ef28163e4e
Батыр <braham.2020@mail.ru>
2022-11-04 14:48:23 +0500

    auto

--- Lab3_Braham_version/de.cpp
+++ Lab3_Braham_version/de.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include <fstream>
 #include <bitset>
 #include<map>
 
@@ -23,10 +24,26 @@ main(){
       text.push_back(a);
    }
     
-   int decod[3] = {208, 198, 213};
-   for(int i = 0; i < text.size(); i++){
-      int y = (decod[i % 3] ^ text[i]);
-      if(y == 32) cout <<" ";
-      else cout << h[y - 192];
+   int decod[3];
+   char buffer[4];
+   ifstream f;
+   f.open("keys.txt");
+   while(1){
+      for(int i=0; i<3;i++){
+         f.getline(buffer, 3, ' ');
+         decod[i]=atoi(buffer);
+      }
+      for(int j = 0; j < 3;j++){
+         int temp;
+         for(int i = 0; i < text.size(); i++){
+            int y = (decod[i % 3] ^ text[i]);
+            if(y == 32) cout <<" ";
+            else cout << h[y - 192];
+         }
+         temp = decod[0];
+         decod[0] = decod[2];
+         decod[2] = decod[1];
+         decod[1] = temp;
+      }
    }
 }
